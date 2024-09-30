ba9ea95a9b155514c907c229e02460c93bd48905
Maksim <maxprofix@gmail.com>
2024-09-30 18:51:50 +0300

    Update Contacts.tsx

--- src/components/Home/Footer/Contacts.tsx
+++ src/components/Home/Footer/Contacts.tsx
@@ -7,7 +7,7 @@ import { BiSolidPhoneCall } from "react-icons/bi";
 const Contacts = () => {
   return (
     <Background imageUrl="/contact.jpg">
-      <div className="w-full h-full py-64 px-20 bg-black bg-opacity-50 text-white flex flex-col gap-10 items-start">
+      <div className="w-full h-full py-64 px-10 bg-black bg-opacity-50 text-white flex flex-col gap-10 items-start">
         <h1 className="text-2xl tracking-wide font-semibold">Наши контакты</h1>
         <div className="text-xl font-light">
           <div>+7 (925) 143-79-52</div>
