553e24ab27a171c3b32145db43a4468be94b6dc3
Maksim <maxprofix@gmail.com>
2024-10-02 11:39:44 +0300

    Update Info.tsx

--- src/components/Intro/Info.tsx
+++ src/components/Intro/Info.tsx
@@ -5,7 +5,7 @@ interface InfoProps {
 
 const Info: React.FC<InfoProps> = ({ title, description }) => {
   return (
-    <div className="text-center max-w-3xl mx-auto flex flex-col gap-4 mt-28 tablet:mt-40 laptop:mt-52 px-4">
+    <div className="text-center max-w-3xl mx-auto flex flex-col gap-4 mt-48 tablet:mt-40 laptop:mt-52 px-4">
       <h2 className="text-4xl tablet:text-5xl laptop:text-6xl tracking-wider font-semibold mb-2 phone:mb-4">
         {title}
       </h2>
