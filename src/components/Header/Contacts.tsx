e72534c1ceb3be098746de9a2049658aa312af32
Maksim <maxprofix@gmail.com>
2024-10-08 16:11:46 +0300

    фыфвыф

--- src/components/Header/Contacts.tsx
+++ src/components/Header/Contacts.tsx
@@ -7,7 +7,7 @@ const Contacts = () => {
       <Socials />
       <div className="flex flex-col smallLaptop:flex-row mt-2 tablet:mt-0 gap-4 tablet:gap-2 tablet:hidden laptop:flex">
         <Link
-          href="tel:+79251437952"
+          href="tel:+74951252527"
           className="w-full text-center py-2 px-4 font-semibold text-base tablet:text-sm border-[3px] rounded-2xl border-yellow-500 smallLaptop:w-[175px]"
         >
           +7 (495) 125-25-27
