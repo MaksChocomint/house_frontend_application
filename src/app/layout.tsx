229fad48023049d322fc8ed193973fc0a72fd3f9
Maksim <maxprofix@gmail.com>
2024-10-25 12:21:10 +0300

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter_Tight } from "next/font/google";
 import "./globals.css";
-import ScriptComponent from "@/components/Script";
 
 export const metadata: Metadata = {
   title: "Flatpoint",
@@ -17,8 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <ScriptComponent />
-
       <body className={`${interTight.className} antialiased overflow-x-hidden`}>
         {children}
       </body>
--- src/components/Intro/Booking.tsx
+++ src/components/Intro/Booking.tsx
@@ -1,6 +1,45 @@
+"use client";
+
+import { useEffect } from "react";
 import "./Booking.css";
 
 const Booking = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.id = "booking";
+    script.innerHTML = `
+      (function(w) {
+        var q = [
+          ['setContext', 'TL-INT-flat-point-ru_2024-10-18', 'ru'],
+          ['embed', 'search-form', {
+              container: 'tl-search-form'
+          }],
+        ];
+        var h=["ru-ibe.tlintegration.ru","ibe.tlintegration.ru","ibe.tlintegration.com"];
+        var t = w.travelline = (w.travelline || {}),
+        ti = t.integration = (t.integration || {});
+        ti.__cq = ti.__cq? ti.__cq.concat(q) : q;
+       if (!ti.__loader) {
+          ti.__loader = true;
+          var d=w.document,c=d.getElementsByTagName("head")[0]||d.getElementsByTagName("body")[0];
+          function e(s,f) {return function() {w.TL||(c.removeChild(s),f())}}
+          (function l(h) {
+              if (0===h.length) return; var s=d.createElement("script");
+              s.isReloadFuncSet = "set";s.type="text/javascript";s.async=!0;s.src="https://"+h[0]+"/integration/loader.js";
+              s.onerror=s.onload=e(s,function(){l(h.slice(1,h.length))});c.appendChild(s)
+          })(h);
+      }
+    })(window);
+    `;
+
+    document.body.appendChild(script);
+
+    return () => {
+      // Очистка скрипта при размонтировании компонента
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div id="block-search">
       <div id="tl-search-form" className="tl-container">
@@ -10,7 +49,7 @@ const Booking = () => {
             rel="nofollow"
             target="_blank"
           >
-            TravelLine
+            Загрузка...
           </a>
         </noindex>
       </div>
