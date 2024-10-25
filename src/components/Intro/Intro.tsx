b802504c1a7650bd46db898610a5e75e69661e6a
Maksim <maxprofix@gmail.com>
2024-10-25 11:33:41 +0300

    Update Intro.tsx

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -19,7 +19,7 @@ const Intro: React.FC<IntroProps> = ({
     <div>
       {darker && <div className="absolute inset-0 bg-black opacity-15 z-10" />}
       <Background imageUrl={imageUrl}>
-        <div className="flex flex-col items-center w-full h-full justify-between phone:gap-6 tablet:gap-8 laptop:gap-10">
+        <div className="flex flex-col items-center w-full h-full justify-between gap-4 tablet:gap-8 laptop:gap-10 overflow-y-auto">
           <Info title={infoTitle} description={infoDescription} />
           <Booking />
         </div>
