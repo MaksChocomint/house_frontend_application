fd823db772fc1b423481d05b5954f69af32fde31
Maksim <maxprofix@gmail.com>
2024-11-10 20:11:20 +0300

    aa

--- prisma/locations.ts
+++ prisma/locations.ts
@@ -1,29 +1,29 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// // eslint-disable-next-line @typescript-eslint/no-require-imports
+// const { PrismaClient } = require("@prisma/client");
+// const prisma = new PrismaClient();
 
-async function main() {
-  try {
-    // Создаем локацию "Москва"
-    const moscow = await prisma.location.create({
-      data: {
-        name: "Москва",
-      },
-    });
+// async function main() {
+//   try {
+//     // Создаем локацию "Москва"
+//     const moscow = await prisma.location.create({
+//       data: {
+//         name: "Москва",
+//       },
+//     });
 
-    // Создаем локацию "Московская область"
-    const moscowRegion = await prisma.location.create({
-      data: {
-        name: "Московская область",
-      },
-    });
+//     // Создаем локацию "Московская область"
+//     const moscowRegion = await prisma.location.create({
+//       data: {
+//         name: "Московская область",
+//       },
+//     });
 
-    console.log("Созданы локации:", moscow, moscowRegion);
-  } catch (error) {
-    console.error("Ошибка при создании локаций:", error);
-  } finally {
-    await prisma.$disconnect();
-  }
-}
+//     console.log("Созданы локации:", moscow, moscowRegion);
+//   } catch (error) {
+//     console.error("Ошибка при создании локаций:", error);
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// }
 
-main();
+// main();
