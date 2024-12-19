1a0be17727f50d6a23b95b2f432f480b8df8ec09
MaksimChocomint <maxprofix@gmail.com>
2024-12-19 18:58:28 +0300

    aaa

--- src/components/Home/Main/Entertainment.tsx
+++ src/components/Home/Main/Entertainment.tsx
@@ -7,6 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { useWindowSize } from "@/utils/useWindowSize";
 import { Pagination } from "swiper/modules";
+import { motion } from "framer-motion";
 import "./Gallery.css";
 import Link from "next/link";
 
@@ -31,7 +32,7 @@ const Entertainment = () => {
       id: 3,
       src: "/main/entertaiment/furako.png",
       title: "Фурако парк",
-      textColor: "text-black",
+      textColor: "text-white",
     },
     {
       id: 4,
@@ -55,7 +56,7 @@ const Entertainment = () => {
       id: 7,
       src: "/main/entertaiment/monastery.png",
       title: "Воскресенский Ново-Иерусалимский монастырь",
-      textColor: "text-black",
+      textColor: "text-white",
     },
     {
       id: 8,
@@ -67,7 +68,6 @@ const Entertainment = () => {
 
   return (
     <Container styles="mt-32 bg-human-detail py-10">
-      {/* Title: для мобильных отдельно */}
       {isMobile && (
         <Title
           upperText="Досуг"
@@ -76,7 +76,6 @@ const Entertainment = () => {
         />
       )}
 
-      {/* Для десктопов и планшетов: грид */}
       {!isMobile && (
         <div className="grid grid-cols-[2fr_1.5fr_1.3fr_1fr] grid-rows-[1fr_1fr_1fr_1.5fr] gap-[6px] smallLaptop:gap-4 h-[750px]">
           <Title
@@ -85,7 +84,7 @@ const Entertainment = () => {
             styles="col-start-1 col-end-2 row-start-1 row-end-2 self-center text-wrap break-words"
           />
           {items.map((item, index) => (
-            <div
+            <motion.div
               key={item.id}
               className={`relative rounded-lg overflow-hidden shadow-lg ${
                 index === 0
@@ -104,19 +103,24 @@ const Entertainment = () => {
                   ? "row-start-4 row-end-5 col-start-3 col-end-4"
                   : "row-start-3 row-end-5 col-start-4 col-end-5"
               }`}
+              whileHover={{ scale: 1.05 }}
             >
               <Image src={item.src} alt={item.title} fill objectFit="cover" />
-              <div
-                className={`absolute bottom-2 left-2 font-medium text-lg ${item.textColor}`}
+              <motion.div
+                className="absolute inset-0 bg-natural-style bg-opacity-60 flex items-center justify-center opacity-0"
+                whileHover={{ opacity: 1 }}
               >
-                {item.title}
-              </div>
-            </div>
+                <div
+                  className={`text-center font-bold text-base smallLaptop:text-xl cursor-default ${item.textColor}`}
+                >
+                  {item.title}
+                </div>
+              </motion.div>
+            </motion.div>
           ))}
         </div>
       )}
 
-      {/* Для мобильных: Swiper */}
       {isMobile && (
         <Swiper
           spaceBetween={16}
@@ -124,14 +128,14 @@ const Entertainment = () => {
           loop
           pagination={{
             clickable: true,
-            el: ".custom-pagination", // Кастомный класс для стилизации пагинации
+            el: ".custom-pagination",
           }}
           modules={[Pagination]}
           className="mySwiper"
         >
           {items.map((item) => (
             <SwiperSlide key={item.id}>
-              <div className="relative mt-10 h-[380px] rounded-lg overflow-hidden shadow-lg">
+              <div className="relative mt-10 h-[450px] rounded-lg overflow-hidden shadow-lg">
                 <Image
                   src={item.src}
                   alt={item.title}
@@ -139,10 +143,12 @@ const Entertainment = () => {
                   objectFit="cover"
                   className="rounded-lg"
                 />
-                <div
-                  className={`absolute bottom-4 left-4 font-medium text-xl ${item.textColor}`}
-                >
-                  {item.title}
+                <div className="absolute bg-natural-style bg-opacity-60 bottom-0 h-1/6 w-full flex items-center justify-center">
+                  <div
+                    className={`text-center font-bold text-xl ${item.textColor}`}
+                  >
+                    {item.title}
+                  </div>
                 </div>
               </div>
             </SwiperSlide>
@@ -150,6 +156,7 @@ const Entertainment = () => {
           <div className="custom-pagination flex justify-center mt-4"></div>
         </Swiper>
       )}
+
       <Link
         href="/accommodation/green-lounge"
         className="uppercase font-medium w-full justify-center smallTablet:justify-end flex gap-4 text-lg laptop:text-xl text-home-coziness items-center mt-8"
