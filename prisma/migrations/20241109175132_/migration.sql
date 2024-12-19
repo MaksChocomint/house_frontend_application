5c16a5c7356fe0f780674ba1b02e8d03541d2a18
MaksimChocomint <maxprofix@gmail.com>
2024-12-19 12:22:52 +0300

    aa

--- prisma/migrations/20241109175132_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `rooms` to the `Apartment` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Apartment" ADD COLUMN     "rooms" INTEGER NOT NULL;
