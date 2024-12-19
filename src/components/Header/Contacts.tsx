3d97d5badd9cb0c99783c89e7a906a9dadef0220
MaksimChocomint <maxprofix@gmail.com>
2024-12-19 16:21:18 +0300

    aaa

--- src/components/Header/Contacts.tsx
+++ src/components/Header/Contacts.tsx
@@ -5,7 +5,7 @@ const Contacts = () => {
   return (
     <div className="flex flex-col tablet:flex-row items-center tablet:mt-0 gap-4">
       <Socials />
-      <div className="flex flex-col smallLaptop:flex-row mt-2 tablet:mt-0 gap-4 tablet:gap-2 tablet:hidden laptop:flex">
+      <div className="flex flex-col smallLaptop:flex-row mt-2 tablet:mt-0 gap-4 tablet:gap-2 tablet:hidden desktop:flex">
         <Link
           href="tel:+74951252527"
           className="w-full font-medium text-center py-2 px-4 text-base text-nowrap"
